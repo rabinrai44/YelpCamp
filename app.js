21f519541b8a83cb2df937e096f260ed813a4973
Rabin Rai <rabinrai44@gmail.com>
2019-04-07 01:09:36 -0400

    Create a campground locally and redirect to campgrounds

--- app.js
+++ app.js
@@ -11,6 +11,22 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
+// Global variable - temporary data
+let campgrounds = [
+  {
+    name: 'Campground 1',
+    image: 'https://farm9.staticflickr.com/8235/8432294727_73ccb892f7.jpg'
+  },
+  {
+    name: 'Campground 2',
+    image: 'https://farm4.staticflickr.com/3617/3389236883_ef334dc46c.jpg'
+  },
+  {
+    name: 'Campground 3',
+    image: 'https://farm6.staticflickr.com/5786/20607281024_5c7b3635cc.jpg'
+  }
+];
+
 /********* ROUTING ********/
 //GET - /
 app.get('/', (req, res) => {
@@ -19,21 +35,26 @@ app.get('/', (req, res) => {
 
 //GET - /campgrounds
 app.get('/campgrounds', (req, res) => {
-  let campgrounds = [
-    {
-      name: 'Campground 1',
-      image: 'https://farm9.staticflickr.com/8235/8432294727_73ccb892f7.jpg'
-    },
-    {
-      name: 'Campground 2',
-      image: 'https://farm4.staticflickr.com/3617/3389236883_ef334dc46c.jpg'
-    },
-    {
-      name: 'Campground 3',
-      image: 'https://farm6.staticflickr.com/5786/20607281024_5c7b3635cc.jpg'
-    }
-  ];
   res.render('campgrounds', { campgrounds: campgrounds });
+});
+
+//POST - /campgrounds
+app.post('/campgrounds', (req, res) => {
+  // hold the incoming data in local variable
+  let name = req.body.name;
+  let image = req.body.image;
+  let newCampground = { name: name, image: image };
+
+  // save Campground to the campgrounds
+  campgrounds.push(newCampground);
+
+  // redirect to the campgrounds route
+  res.redirect('/campgrounds');
+});
+
+//GET - campgrounds/new
+app.get('/campgrounds/new', (req, res) => {
+  res.render('new');
 });
 
 // App listen
